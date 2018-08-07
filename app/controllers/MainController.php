e0943cc148472b19ed81c31d9f21753a3e08282c
Lancevrot <33875002+Lancevrot@users.noreply.github.com>
2018-08-07 14:29:01 +0300

    Add hits

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -10,7 +10,9 @@ class MainController extends AppController
     
     public function indexAction(){
         $brands = \R::find('brand', 'LIMIT 3');
-        $this->set(compact('brands'));
+        $hits = \R::find("product", "hit = '1' AND status = '1' LIMIT 8");
+        $this->set(compact('brands', 'hits'));
+       
 
         $this->setMeta('Main Page', 'Description', 'Keywords');
 
