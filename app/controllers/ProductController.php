dacd4ee16dd7d97b27c1fc1779759fccd82c8510
Lancevrot <33875002+Lancevrot@users.noreply.github.com>
2018-08-09 21:55:09 +0300

    add related

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -1,15 +1,34 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: avolo
- * Date: 09-Aug-18
- * Time: 14:06
- */
 
 namespace app\controllers;
 
 
-class ProductController
+class ProductController extends AppController
 {
 
+    public function viewAction(){
+        $alias = $this->route['alias'];
+        $product = \R::findOne('product', "alias = ? AND status = '1'", [$alias]);
+        if(!$product){
+            throw new \Exception('Cant found page', 404);
+        }
+
+        // BREADCRUMBS
+
+        // connected products
+
+        $related = \R::getAll("SELECT * FROM related_product JOIN product ON product.id=related_product.related_id WHERE related_product.product_id = ?", [$product->id]);
+
+        //Add cookies to the asked product
+
+        //viewed products
+
+        //gallery
+        $gallery =\R::findAll('gallery', 'product_id = ?', [$product->id]);
+        //debug($gallery);
+        //modifcations
+
+        $this->setMeta($product->title, $product->description, $product->keywords);
+        $this->set(compact('product', 'related', 'gallery'));
+    }
 }
